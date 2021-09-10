292cd0923b7fdbab291096e8b8942d5dd782e271
Alexander de Groot <alexnhdg@gmail.com>
2021-09-10 16:22:30 -0700

    Added initial ui and server functions for app

--- 2021_w33_bea-infrastructure-investment/2021_w33_tidytuesday.R
+++ 2021_w33_bea-infrastructure-investment/2021_w33_tidytuesday.R
@@ -20,6 +20,52 @@ chain_investment <- tuesdata$chain_investment
 dash_data_chained <- chain_investment %>%
   filter(group_num %in% c(1, 2, 3, 4, 16, 17, 22))
 
+
+# User Interface ----------------------------------------------------------
+
+ui <- fluidPage(
+    
+    titlePanel("United States Historical Infrastucture Investment Dashboard"),
+    
+    sidebarPanel(
+        
+        textInput(
+            inputId = "plot_title",
+            label = "Plot Title:",
+            value = "Title goes here",
+            placeholder = "Enter a plot title"
+        ),
+        
+        sliderInput(
+            inputId = "selected_years",
+            label = "Year Range:",
+            min = min(dash_data_chained$year), max = max(dash_data_chained$year) ,
+            value = c(min(dash_data_chained$year), max(dash_data_chained$year)),
+            step = 1
+        ),
+        
+        checkboxGroupInput(
+            inputId = "selected_meta_groups",
+            label = "Infrastrucutre Type:",
+            choices = unique(dash_data_chained$meta_cat),
+            selected = unique(dash_data_chained$meta_cat)
+        ),
+        
+        selectInput(
+            inputId = "selected_sub_groups",
+            label = "Infrastructure Sub-Group:",
+            choices = unique(dash_data_chained$category),
+            selected = NULL,
+            multiple = TRUE,
+            selectize = TRUE
+        )
+    ),
+    
+    mainPanel(
+        
+        plotOutput(outputId = "plot")
+        
+    )
 )
 
 
@@ -27,115 +73,32 @@ dash_data_chained <- chain_investment %>%
 
 server <- function(input, output) {
     
+    plot_data_chained <- reactive({
+        dash_data_chained %>% 
+            filter(
+                year >= input$selected_years[1],
+                year <= input$selected_years[2],
+                meta_cat %in% input$selected_meta_groups,
+                category %in% input$selected_sub_groups
+            )
+    })
+    
+    output$plot <- renderPlot({
+        ggplot(plot_data_chained(), aes(x = year, y = gross_inv_chain)) +
+            geom_area(aes(fill = category), color = "white") +
+            labs(
+                title = input$plot_title,
+                caption = "Source: US Bureau of Labor Economics",
+                x = "Year", y = "US Dollars"
+            ) +
+            theme_minimal()
+    })
+
 }
 
 
 # Run Shiny App -----------------------------------------------------------
 
 shinyApp(ui = ui, server = server)
-
-
-
-
-# get data ----------------------------------------------------------------
-
-# chained investment data by state
-chained_inv_data_raw <- read_excel(
-    path = "week-33_bea-infrastructure-investment/infrastructure-data-may-2020.xlsx",
-    sheet = "By state data",
-    range = "B117:AB169"
-)
-
-chained_inv_data <- chained_inv_data_raw %>% 
-    rename(state = ...1) %>% 
-    mutate(state = str_squish(state)) %>% 
-    filter(state != "S&L Highways") %>% 
-    pivot_longer(
-        cols = `1992`:`2017`,
-        names_to = "year",
-        values_to = "gross_inv_chained",
-        names_transform = list(year = as.integer)
-    )
-
-
-# population data
-state_pop_raw <- read_excel(
-    path = "week-33_bea-infrastructure-investment/infrastructure-data-may-2020.xlsx",
-    sheet = "By state data",
-    range = "B174:AB226"
-)
-
-
-state_pop <- state_pop_raw %>% 
-    rename(state = ...1) %>% 
-    mutate(state = str_squish(state)) %>% 
-    filter(state != "United States") %>% 
-    pivot_longer(
-        cols = `1992`:`2017`,
-        names_to = "year",
-        values_to = "population",
-        names_transform = list(year = as.integer)
-    )
-
-
-# highway and street infrastructure age data
-# hwy_age_raw <- read_excel(
-#     path = "week-33_bea-infrastructure-investment/infrastructure-data-may-2020.xlsx",
-#     sheet = "Avg age govt FA",
-#     range = "A7:CR81",
-#     na = "....."
-# )
-# 
-# hwy_age <- hwy_age_raw %>% 
-#     rename(investment = ...2) %>% 
-#     mutate(investment = str_squish(investment)) %>% 
-#     filter(Line == "67") %>% 
-#     select(`1925`:`2018`) %>% 
-#     pivot_longer(
-#         cols = `1925`:`2018`,
-#         names_to = "year",
-#         values_to = "hwy_age",
-#         names_transform = list(year = as.integer)
-#     )
-
-
-# prepare date for charting -----------------------------------------------
-
-hwy_inv_data <- chained_inv_data %>% 
-    left_join(state_pop, by = c("state", "year")) %>% 
-    # left_join(hwy_age, by = "year") %>% 
-    mutate(
-        inv_per_capita = gross_inv_chained / population
-    ) %>% 
-    group_by(state) %>% 
-    mutate(
-        label = if_else(year == max(year), state, NA_character_)
-    )
-
-
-# make chart --------------------------------------------------------------
-
-ggplot(hwy_inv_data, aes(x = year, y = inv_per_capita, color = state)) +
-    geom_line(size = 2) +
-    scale_y_continuous(labels = scales::dollar_format()) +
-    scale_x_continuous(breaks = seq(1992, 2017, 3)) +
-    gghighlight(state == "{closest_state}")
-    labs(
-        title = "{closest_state} Highway Infrastructure Investment Per Capita, 1992-2017",
-        subtitle = "US Dollars, Millions (2012 Adjusted)",
-        x = "",
-        y = "",
-        caption = "Source: United States Bureau of Economic Analysis"
-    ) +
-    theme_minimal() +
-    theme(
-        legend.position = "none"
-    ) +
-    transition_states(
-        states = state,
-        transition_length = 1,
-        state_length = 2,
-        wrap = TRUE
-    )
 
 
