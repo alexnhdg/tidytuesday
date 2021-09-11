0405926523da3b09f2e047e501c92cf335da5db5
Alexander de Groot <alexnhdg@gmail.com>
2021-09-10 23:40:57 -0700

    Update data for use in {shiny} app

--- 2021_w33_bea-infrastructure-investment/2021_w33_tidytuesday.R
+++ 2021_w33_bea-infrastructure-investment/2021_w33_tidytuesday.R
@@ -7,6 +7,49 @@
 
 library(tidyverse)
 library(shiny)
+library(readxl)
+library(here)
+
+# Get Data ----------------------------------------------------------------
+
+inv_gross_data_raw <- read_xlsx(
+    path = here("2021_w33_bea-infrastructure-investment", "infrastructure-data-may-2020.xlsx"),
+    sheet = "cu$inv",
+    range = "A3:BU108",
+    na = ""
+)
+
+
+# Tidy Data ---------------------------------------------------------------
+
+inv_data <- inv_gross_data_raw %>% 
+    rename(group_num = `...1`, category = `...2`) %>% 
+    mutate(
+        meta_cat = ifelse(!is.na(group_num), category, NA_character_)
+    ) %>% 
+    fill(group_num, meta_cat) %>% 
+    group_by(group_num) %>% 
+    slice(-1) %>% 
+    ungroup() %>% 
+    pivot_longer(
+        cols = `1947`:`2017`,
+        names_to = "year",
+        names_transform = c(year = as.integer),
+        values_to = "inv_gross"
+    ) %>% 
+    filter(group_num %in% c(1, 2, 3, 4, 16, 17, 22)) %>% 
+    mutate(
+        meta_cat = case_when(
+            group_num == 1 ~ "Total infrastructure by type",
+            group_num == 2 ~ "Total infrastructure by funding source",
+            group_num == 3 ~ "Basic infrastructure by funding source",
+            group_num == 4 ~ "Basic infrastructure by type",
+            group_num == 16 ~ "Social infrastructure by funding source",
+            group_num == 17 ~ "Social infrastructure by type",
+            group_num == 22 ~ "Digital infrastructure by type"
+        )
+    ) %>% 
+    select(meta_cat, category, year, inv_gross)
 
 
 # Get Data ----------------------------------------------------------------
