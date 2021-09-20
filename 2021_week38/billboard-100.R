5553777fe0f767748f242ebf98bde5317d0ef2f5
Alexander de Groot <alexnhdg@gmail.com>
2021-09-19 23:30:05 -0700

    Remove audio features data

--- 2021_week38/billboard-100.R
+++ 2021_week38/billboard-100.R
@@ -16,9 +16,6 @@ library(lubridate)
 # 
 # billboard_raw <- tuesdata$billboard
 # write_csv(billboard_raw, here::here("2021_week38", "billboard_raw.csv"))
-# 
-# audio_features_raw <- tuesdata$audio_features
-# write_csv(audio_features_raw, here::here("2021_week38", "audio_features_raw.csv"))
 
 billboard_raw <- read_csv(
     file = here::here("2021_week38", "billboard_raw.csv"),
@@ -35,35 +32,6 @@ billboard_raw <- read_csv(
         weeks_on_chart = col_double()
     )
 )
-
-audio_features_raw <- read_csv(
-    file = here::here("2021_week38", "audio_features_raw.csv"),
-    col_types = cols(
-        song_id = col_character(),
-        performer = col_character(),
-        song = col_character(),
-        spotify_genre = col_character(),
-        spotify_track_id = col_character(),
-        spotify_track_preview_url = col_character(),
-        spotify_track_duration_ms = col_double(),
-        spotify_track_explicit = col_logical(),
-        spotify_track_album = col_character(),
-        danceability = col_double(),
-        energy = col_double(),
-        key = col_double(),
-        loudness = col_double(),
-        mode = col_double(),
-        speechiness = col_double(),
-        acousticness = col_double(),
-        instrumentalness = col_double(),
-        liveness = col_double(),
-        valence = col_double(),
-        tempo = col_double(),
-        time_signature = col_double(),
-        spotify_track_popularity = col_double()
-    )
-)
-
 
 # Tidy Data ---------------------------------------------------------------
 
